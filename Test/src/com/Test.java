4de01b6179b0731da45de56ac7dba090d88aef6b
sachingite <sachin.gite@gmail.com>
2015-03-31 18:41:35 +0530

    qwsqwsqs

--- Test/src/com/Test.java
+++ Test/src/com/Test.java
@@ -1,5 +1,12 @@
 package com;
 
-public class Test {
+public class Test implements Comparable<Test> {
+	private String s;
+	@Override
+	public int compareTo(Test o) {
+		// TODO Auto-generated method stub
+		return s.compareTo(o.s);
+	}
+	
 
 }
--- Test/src/com/Test1.java
+++ Test/src/com/Test1.java
@@ -1,9 +1,18 @@
 package com;
 
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Test1 {
   
   public static void main(String[] args) {
     System.out.println("This is test");
+    
+    Comparator<Test> t = (Test a, Test b) -> { return a.compareTo(b);};
+    Set<Test> test = new TreeSet<Test>(t);
+    
+    
   }
 
 }
